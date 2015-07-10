bb02da0c4eb144880bce3240aec90e6f2856d8b5
Béchir TALEB ALI <btalebali@prologue.fr>
2015-07-10 16:34:36 +0200

    updates

--- src/vcclient/TestVC_CLIENT.java
+++ src/vcclient/TestVC_CLIENT.java
@@ -9,15 +9,15 @@ public class TestVC_CLIENT {
 		// TODO Auto-generated method stub
 	
 		
-/*//For PROlogue's Internal vCloud Director		
+//For PROlogue's Internal vCloud Director		
 		Object[] argsvCconfig= new Object[5];
 		argsvCconfig[0]  = "https://172.17.117.108/";   // URL
 		argsvCconfig[1]  = "uicbm";                     // user
 		argsvCconfig[2]  = "prologue";	                // Organization 
 		argsvCconfig[3]  = "u15i21cb21m0";              //password
 		argsvCconfig[4]  = "5.1"; //1.5 // 5.5		    //allowed parameters
-*/
-		
+
+	/*	
 // For Alhambra's vCloud Director		
 		Object[] argsvCconfig= new Object[5];
 		argsvCconfig[0]  = "https://clouddemo.a-e.es/";   // URL
@@ -26,24 +26,24 @@ public class TestVC_CLIENT {
 		argsvCconfig[3]  = "prologue";              //password
 		argsvCconfig[4]  = "5.1"; //1.5 // 5.5 //allowed parameters
 	
-		
+	*/	
 		
 		Object[] argsvCloud= new Object[21];		
 		// user,password,organization,vDC,host,version,image,VMname,VMid,hostname,password,flavor,label,firewallrules,
 		// profile,access,private_address,public_address, MAC address, VMstatus		
 		
 		argsvCloud[0]  = "instanceid";   				            // VM id 
-		argsvCloud[1]  = "vDC-PROLOGUE-CLOUDPORT";//"vDC_prologue";  							//vDC,
+		argsvCloud[1]  = "vDC_prologue";//"";  vDC-PROLOGUE-CLOUDPORT							//vDC,
 		argsvCloud[2]  = "Linux";  									//catalogue
-		argsvCloud[3]  ="Ubuntu12.04 x86_64";//"Ubuntu12.04 x86_64";         				//vApptemplate
-		argsvCloud[4]  = "VM04";   	              				//vApp name
+		argsvCloud[3]  ="Windows 2008 server R2-test";//"Ubuntu12.04 x86_64";         				//vApptemplate
+		argsvCloud[4]  = "VM-Testwindows2";   	              				//vApp name
 		argsvCloud[5]  = "urn:vcloud:vm:7c1a5933-f718-4e9c-be95-7f7e815d7426";	//vAppId of the VM in vCloud platform,						                        
 		argsvCloud[6]  = "uicbm";					                            //hostname,
-		argsvCloud[7]  = "azdFgtpld";					                        //rootpass, if null , we generate a random rootpass
-		argsvCloud[8]  = "1";				 	                        //cpu(N° vcpu)	
-		argsvCloud[9]  = "512";				 	                        //ram (MHz) foe exemple 2048
-		argsvCloud[10] = "16";				 	                        //disc(Go)
-		argsvCloud[11] = "EDG-INET01";  //"EdgeGW";				 	                        //EdgeGateway
+		argsvCloud[7]  = "azdFgt154pld";					                        //rootpass, if null , we generate a random rootpass
+		argsvCloud[8]  = "3";				 	                        //cpu(N° vcpu)	
+		argsvCloud[9]  = "2048";				 	                        //ram (MHz) foe exemple 2048
+		argsvCloud[10] = "40";				 	                        //disc(Go)
+		argsvCloud[11] = "EdgeGW";  //"";	EDG-INET01			 	                        //EdgeGateway
 		argsvCloud[12] = "vLAN01:20.10.10.0/24:subnet1:20.10.10.128/26:8.8.4.4"; //label for network
 		
 		argsvCloud[13] = "cosacs:8286:8286:tcp:0.0.0.0/0:inout*"
@@ -55,7 +55,7 @@ public class TestVC_CLIENT {
 		            + "RDP:3389:3389:tcp:172.17.161.30/32:inout*"
 					+ "ACCORDS:8000:8005:tcp:0.0.0.0/0:inout";  // security group for this VM
 		
-		argsvCloud[14] = "private";              //access 		
+		argsvCloud[14] = "public";              //access 		
 		argsvCloud[15] = "notset";               //private address
 		argsvCloud[16] = "notset";               //public address
 		argsvCloud[17] = "notset";               //MAC address
@@ -78,6 +78,7 @@ public class TestVC_CLIENT {
 		aux = append(aux, argsvCloud[1]);
 		
 		//String[] vApptemplate = (String[]) ManageIaaS.get_vApptemplatepervDC(aux);
+		//System.out.println(vApptemplate[0]);
 		
 		
 		//String[] EDGWs = (String[]) ManageIaaS.get_EDGEpervDC(aux);
